e4b151129d27f997be70ad5ceb7602bd886eed1c
CBedoy <carlos.bedoy@gmail.com>
2014-02-26 17:32:04 -0600

    Create class for current services

---
+++ src/com/uandroides/aprende/servicios/obtainAllTemas.java
@@ -0,0 +1,75 @@
+package com.uandroides.aprende.servicios;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
+
+import com.uandroides.aprende.utils.RESTClient;
+import com.uandroides.aprende.vistas.MainActivity;
+
+public class obtainAllTemas extends AsyncTask<String, Integer, String> implements IServiceConector{
+	private ProgressDialog pd;
+	private Context contexto;
+	private final String url="";
+	
+	
+	public obtainAllTemas(Context contexto){
+		this.contexto = contexto;
+	}
+	
+	
+	@Override
+	protected void onPreExecute(){
+		pd = ProgressDialog.show(contexto, "", "Cargando...");
+	}
+	
+	@Override
+	protected String doInBackground(String... arg0) {
+		
+		return null;
+	}
+	
+	@Override
+	public String correrServicio(Object objeto) {
+		
+		try{
+			RESTClient request = new RESTClient(url);
+			String respuesta = MainActivity.mthis.shared.getString("estadisticas", null);
+			if(request.getResponse()!=null){
+				Editor edit = MainActivity.mthis.shared.edit();
+				edit.putString("estadisticas", request.getResponse());
+				edit.commit();
+				return request.getResponse();
+				
+			}
+			return respuesta;
+		}catch(Exception e){
+			e.printStackTrace();
+			String responsedsavedd = MainActivity.mthis.shared.getString("estadisticas", null);
+			return responsedsavedd;
+		
+		}
+	}
+	
+	
+	
+	@Override
+	protected void onPostExecute(String respuesta){
+		pd.dismiss();
+		try {
+			JSONObject json = new JSONObject(respuesta);
+			
+			
+			
+			
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			
+		}
+	}
+}
