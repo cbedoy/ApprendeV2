1c81c2bcf95ae59f593778ecaef5b1081203aee3
CBedoy <carlos.bedoy@gmail.com>
2014-02-25 18:21:22 -0600

    Add model user, implement service for createUser

---
+++ src/com/uandroides/aprende/modelos/APPUsuario.java
@@ -0,0 +1,65 @@
+package com.uandroides.aprende.modelos;
+
+public class APPUsuario {
+	private int id;
+	private String correo;
+	private String contrasena;
+	private String nombre;
+	private int edad;
+	private String link;
+	private int puntos;
+	private int pruebas;
+	
+	public APPUsuario(){
+		
+	}
+	
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	public int getPruebas() {
+		return pruebas;
+	}
+	public void setPruebas(int pruebas) {
+		this.pruebas = pruebas;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ src/com/uandroides/aprende/servicios/createUser.java
@@ -0,0 +1,88 @@
+package com.uandroides.aprende.servicios;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.uandroides.aprende.controladores.RESTClient;
+import com.uandroides.aprende.controladores.RESTClient.RequestMethod;
+import com.uandroides.aprende.modelos.APPUsuario;
+import com.uandroides.aprende.vistas.ExamenActivity;
+import com.uandroides.aprende.vistas.MainActivity;
+import com.uandroides.aprende.vistas.PerfilActivity;
+import com.uandroides.aprende.vistas.TemasActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
+
+public class createUser extends AsyncTask<String, Integer, String> implements IServiceConector{
+	private ProgressDialog pd;
+	private Context contexto;
+	private final String url="";
+	public createUser(Context contexto){
+		this.contexto = contexto;
+	}
+	
+	
+	@Override
+	protected void onPreExecute(){
+		pd = ProgressDialog.show(contexto, "", "Cargando...");
+	}
+	
+	@Override
+	protected String doInBackground(String... arg0) {
+		
+		return null;
+	}
+	
+	@Override
+	public String correrServicio(Object objeto) {
+		APPUsuario usuario = (APPUsuario) objeto;
+		try{
+			RESTClient request = new RESTClient(url);
+			request .Execute(RequestMethod.GET);
+			request.AddParam("correo", usuario.getCorreo());
+			request.AddParam("contrasena", usuario.getContrasena());
+			request.AddParam("nombre", usuario.getNombre());
+			request.AddParam("edad", usuario.getEdad()+"");
+			request.AddParam("foto", usuario.getLink());
+			String respuesta = MainActivity.mthis.shared.getString("usuario", null);
+			if(request.getResponse()!=null){
+				Editor edit = MainActivity.mthis.shared.edit();
+				edit.putString("usuario", request.getResponse());
+				edit.commit();
+				return request.getResponse();
+				
+			}
+			return respuesta;
+		}catch(Exception e){
+			e.printStackTrace();
+			String responsedsavedd = MainActivity.mthis.shared.getString("usuario", null);
+			return responsedsavedd;
+		
+		}
+	}
+	
+	
+	
+	@Override
+	protected void onPostExecute(String respuesta){
+		pd.dismiss();
+		try {
+			JSONObject json = new JSONObject(respuesta);
+			
+			
+			
+			
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			
+		}
+	}
+
+
+	
+
+}
