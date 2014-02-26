a1aa8bd7005f87b62f940ea32cf93401facf3a52
CBedoy <carlos.bedoy@gmail.com>
2014-02-26 17:20:22 -0600

    Add class Constantes

---
+++ src/com/uandroides/aprende/modelos/Constantes.java
@@ -0,0 +1,8 @@
+package com.uandroides.aprende.modelos;
+
+public class Constantes {
+	public static String currentUser= "currentUser";
+	public static String allStadistics= "allStadistics";
+	public static String currentExam = "currentExam";
+
+}
