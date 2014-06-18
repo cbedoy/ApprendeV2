1d17c4fa9d09c5bd8dcaff0aaa073fb340e412e2
CBedoy <carlos.bedoy@gmail.com>
2014-06-17 22:25:00 -0500

    Fix crash error

--- src/com/cbedoy/apprende/QuestionaryView.java
+++ src/com/cbedoy/apprende/QuestionaryView.java
@@ -1,12 +1,13 @@
 package com.cbedoy.apprende;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 
 import com.cbedoy.apprende.adapters.QuestionAdapter;
 
-public class QuestionaryView extends ActionBarActivity {
+public class QuestionaryView extends FragmentActivity  {
 
 	private QuestionAdapter 		questionAdapter;
 	private ViewPager 				mViewPager;
